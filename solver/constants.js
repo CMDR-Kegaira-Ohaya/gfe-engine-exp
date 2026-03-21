cd65e7ba0400b80fe87954cda2beee34655a6a2b
CMDR-Kegaira-Ohaya <101864926+CMDR-Kegaira-Ohaya@users.noreply.github.com>
2026-03-21 06:43:32 +0200

    Add solver constants module

---
+++ solver/constants.js
@@ -0,0 +1,16 @@
+export const AXES = Object.freeze(['Cfg', 'Emb', 'Org', 'Dir', 'Leg']);
+export const SIGMA = Object.freeze(['L', 'M', 'Dst']);
+export const UNFOLDING = Object.freeze(['acute', 'accumulated']);
+export const REGISTERS = Object.freeze(['retained', 'emitted']);
+export const MODES = Object.freeze([
+  'load',
+  'route',
+  'overflow',
+  'substitute',
+  'suppress',
+  'amplify',
+  'threshold-transfer'
+]);
+
+export const GROUNDING_AXES = Object.freeze(['Cfg', 'Emb', 'Org']);
+export const DASHBOARD_SCALE = 100;
