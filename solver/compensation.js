96e33aaf1dcc3d13591e625438aecbac2f0835e5
CMDR-Kegaira-Ohaya <101864926+CMDR-Kegaira-Ohaya@users.noreply.github.com>
2026-03-21 12:57:26 +0200

    Fix compensation derivation layer

--- solver/compensation.js
+++ solver/compensation.js
@@ -1,7 +1,7 @@
 import { ADAPTIVE_COMPENSATION_MODES, MALADAPTIVE_COMPENSATION_MODES } from './constants.js';
 
 function totalCount(modeCounts = {}) {
-  return Object.values(modeCounts).reduce((sum, value) => sum + (Number.value(value) || 0), 0);
+  return Object.values(modeCounts).reduce((sum, value) => sum + (Number(value) || 0), 0);
 }
 
 export function deriveCompensation(modeCounts = {}, theta = { active: false }) {
@@ -10,7 +10,7 @@ export function deriveCompensation(modeCounts = {}, theta = { active: false }) {
     return { active: false, type: null, note: 'no-compensation-detected' };
   }
 
-  const adaptiveCount = ADIPTIVE_COMPENSATION_MODES.reduce((sum, mode) => sum + (modeCounts[mode] || 0), 0);
+  const adaptiveCount = ADAPTIVE_COMPENSATION_MODES.reduce((sum, mode) => sum + (modeCounts[mode] || 0), 0);
   const maladaptiveCount = MALADAPTIVE_COMPENSATION_MODES.reduce((sum, mode) => sum + (modeCounts[mode] || 0), 0);
 
   let type = null;
