a82599f89126bae7c349a309b23ff55ddaec4967
CMDR-Kegaira-Ohaya <101864926+CMDR-Kegaira-Ohaya@users.noreply.github.com>
2026-03-21 06:45:18 +0200

    Add solver public index module

---
+++ solver/index.js
@@ -0,0 +1,5 @@
+export { validateCase } from './validate.js';
+export { solveCase, buildEnvelopeSummary, solveParticipantStep, cfgGate } from './engine.js';
+export { buildDashboardChunks } from './dashboard.js';
+export { normalizePayloadEvent, groupEventsByStep } from './payload.js';
+export { DEFAULT_WEIGHTS } from './config.js';
