e959a8eeeace335d1f4e3fa441b02d85ab59a9a5
CMDR-Kegaira-Ohaya <101864926+CMDR-Kegaira-Ohaya@users.noreply.github.com>
2026-03-21 08:50:14 +0200

    Expand solver constants for canonical runtime layers

--- solver/constants.js
+++ solver/constants.js
@@ -1,7 +1,11 @@
 export const AXES = Object.freeze(['Cfg', 'Emb', 'Org', 'Dir', 'Leg']);
+export const GROUNDING_AXES = Object.freeze(['Cfg', 'Emb', 'Org']);
+
 export const SIGMA = Object.freeze(['L', 'M', 'Dst']);
 export const UNFOLDING = Object.freeze(['acute', 'accumulated']);
 export const REGISTERS = Object.freeze(['retained', 'emitted']);
+export const FACES = Object.freeze(['inner', 'outer']);
+
 export const MODES = Object.freeze([
   'load',
   'route',
@@ -12,5 +16,23 @@ export const MODES = Object.freeze([
   'threshold-transfer'
 ]);
 
-export const GROUNDING_AXES = Object.freeze(['Cfg', 'Emb', 'Org']);
+xport const COMPENSATION_MODES = Object.freeze([
+  'overflow',
+  'substitute',
+  'suppress',
+  'threshold-transfer',
+  'route'
+]);
+
+export const ADAPTIVE_COMPENSATION_MODES = Object.freeze([
+  'route',
+  'substitute',
+  'threshold-transfer'
+]);
+
+export const MALADAPTIVE_COMPENSATION_MODES = Object.freeze([
+  'overflow',
+  'suppress'
+]);
+
 export const DASHBOARD_SCALE = 100;
